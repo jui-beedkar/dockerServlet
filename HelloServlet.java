976acf9c6992cc6818651a8b180aa5c5998af952
jui-beedkar <148173607+jui-beedkar@users.noreply.github.com>
2025-06-05 13:39:04 +0530

    Create HelloServlet.java

---
+++ HelloServlet.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello from Servlet in Docker!</h1>");
+    }
+}
